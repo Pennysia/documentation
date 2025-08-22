450dcfd33e8c50cc79d1e60af14b6e209c0e7e45
Settee <ake14243@gmail.com>
2025-08-22 20:41:30 +0700

    config: update VitePress theme configuration
    - Update theme/index.ts with custom theme settings
    - Integrate custom CSS and component overrides
    - Support enhanced theming capabilities

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,14 @@
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './styles/custom.css'
+import { YouTubeEmbed } from '@miletorix/vitepress-youtube-embed' // [!code ++]
+import '@miletorix/vitepress-youtube-embed/style.css' // [!code ++]
 
 export default {
-  ...DefaultTheme
+  extends: DefaultTheme,
+  enhanceApp(ctx) {
+    ctx.app.component('YouTubeEmbed', YouTubeEmbed) // [!code ++]
+  }
 }
 
 
