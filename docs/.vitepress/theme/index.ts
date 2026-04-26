fc487f781200fab9ae0e83453ddaa28012f84eda
Settee <99028384+Settee-ake@users.noreply.github.com>
2026-04-26 17:57:06 +0700

    Update theme index

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,20 +1,18 @@
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './styles/custom.css'
-import { YouTubeEmbed } from '@miletorix/vitepress-youtube-embed' // [!code ++]
-import '@miletorix/vitepress-youtube-embed/style.css' // [!code ++]
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCopy } from '@fortawesome/free-regular-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./styles/custom.css";
+import { YouTubeEmbed } from "@miletorix/vitepress-youtube-embed"; // [!code ++]
+import "@miletorix/vitepress-youtube-embed/style.css"; // [!code ++]
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCopy } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(faCopy)
+library.add(faCopy);
 
 export default {
   extends: DefaultTheme,
   enhanceApp(ctx) {
-    ctx.app.component('YouTubeEmbed', YouTubeEmbed) // [!code ++]
-    ctx.app.component('FontAwesomeIcon', FontAwesomeIcon)
-  }
-}
-
-
+    ctx.app.component("YouTubeEmbed", YouTubeEmbed); // [!code ++]
+    ctx.app.component("FontAwesomeIcon", FontAwesomeIcon);
+  },
+};
