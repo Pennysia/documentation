b1293ac3f11bceb2b495b94dd126e85b0f5ed150
Settee <99028384+Settee-ake@users.noreply.github.com>
2026-04-29 22:45:49 +0700

    theme: register DocLayout with CopyPageButton component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,11 +8,13 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { faCopy } from "@fortawesome/free-regular-svg-icons";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import DocLayout from "./DocLayout.vue";
 
-library.add(faCopy, faCheck);
+library.add(faCopy as any, faCheck as any);
 
 export default {
   extends: DefaultTheme,
+  Layout: DocLayout,
   enhanceApp(ctx: EnhanceAppContext) {
     ctx.app.component("YouTubeEmbed", YouTubeEmbed);
     ctx.app.component("FontAwesomeIcon", FontAwesomeIcon);
